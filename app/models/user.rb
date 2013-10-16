013b5383d0ccef335b9595f088f7d9afbcdd969a
Joe Hilton <jhilton@frontlineutilities.co.uk>
2013-10-16 15:49:26 +0100

    Upload Profile Pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
  devise :database_authenticatable, :registerable,
     :recoverable, :rememberable
 	
-	has_attached_file :photo, :styles => {:small => "230x230>" }
+	has_attached_file :image, :styles => {:small => "230x230>" }
 	
 	#Slugalicious
 	include Slugalicious

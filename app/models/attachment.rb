49fe570dc261b58f2237faacde7d534bf34b5116
Joe Hilton <jhilton@frontlineutilities.co.uk>
2013-10-21 14:56:01 +0100

    New Image Upload Model

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,15 @@
 class Attachment < ActiveRecord::Base
 
-	has_attached_file :attachment, :styles => { :small => "x200>", :medium => "x300>", :large => "x400>"}
+	has_attached_file :attachment,
+		:styles =>  lambda { |a| 
+			if ['image/jpeg', 'image/jpg', 'image/gif', 'image/png'].include? a.instance.attachment_content_type
+				{:small => "x200>", :medium => "x300>", :large => "x400>"}
+			else
+				{}
+			end
+		}
+	validates_presence_of :attachment 
+	
 	has_and_belongs_to_many :categories, :class_name => 'Category'
 	
 	def self.category(id)

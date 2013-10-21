7724897dc700d0b812501e128224734ea5987544
Joe Hilton <jhilton@frontlineutilities.co.uk>
2013-10-21 15:47:18 +0100

    Fixed Image.rb File

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,6 @@
 class Attachment < ActiveRecord::Base
 
-	has_attached_file :attachment, :styles => {:small => "x200>", :medium => "x300>", :large => "x400>"}
+	has_attached_file :attachment, styles:  lambda { |a| a.instance.attachment_content_type =~ %r(image) ? {:small => "x200>", :medium => "x300>", :large => "x400>"}  : {} }
 		
 	
 	has_and_belongs_to_many :categories, :class_name => 'Category'

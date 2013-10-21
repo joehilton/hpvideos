eb1cc8e07893c28dd7c57b19d9fd707ab1e932e2
Joe Hilton <jhilton@frontlineutilities.co.uk>
2013-10-21 15:52:35 +0100

    Fixed JOE's PROBLEM

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,8 +1,6 @@
 class Attachment < ActiveRecord::Base
 
 	has_attached_file :attachment, styles:  lambda { |a| a.instance.attachment_content_type =~ %r(image) ? {:small => "x200>", :medium => "x300>", :large => "x400>"}  : {} }
-		
-	
 	has_and_belongs_to_many :categories, :class_name => 'Category'
 	
 	def self.category(id)
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,5 @@
 class Video < ActiveRecord::Base
 
 	has_attached_file :source
-	:content_type => ['video/mpg']
 
 end

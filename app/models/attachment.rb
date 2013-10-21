1d25a92cfe5fde6f4ca049a127b855f655db4513
Joe Hilton <jhilton@frontlineutilities.co.uk>
2013-10-21 15:26:29 +0100

    Image.rb Improvement

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,18 +1,21 @@
 class Attachment < ActiveRecord::Base
 
 	has_attached_file :attachment,
-		styles:  lambda { |a| 
-			if ['image/jpeg', 'image/jpg', 'image/gif', 'image/png'].include? a.instance.attachment_content_type
-				{:small => "x200>", :medium => "x300>", :large => "x400>"}
-			else
-				{}
-			end
-		} 
+		styles:  lambda { |a| a.instance.attachment_content_type =~ %r(image) ? {:small => "x200>", :medium => "x300>", :large => "x400>"}  : {} }
 	
 	has_and_belongs_to_many :categories, :class_name => 'Category'
 	
 	def self.category(id)
 		joins(:categories).where('`categories`.`id` = ?', id)
+	end
+	
+	protected
+	def is_type_of_video?
+		attachment.content_type =~ %r(video)
+	end
+
+	def is_type_of_image?
+		attachment.content_type =~ %r(image)
 	end
 	
 end
